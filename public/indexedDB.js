40a412c691ff7d1c2f37b73ddd99bbbcce22eb12
Linda Martinez <lindanmjw@gmail.com>
2022-01-31 17:29:24 -0500

    final update for cache fix

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -1,56 +1,59 @@
-// sample code
+const indexedDB =
+    window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB ||
+    window.shimIndexedDB;
 
-function checkForIndexedDb() {
-    if (!window.indexedDB) {
-        console.log("Your browser doesn't support a stable version of IndexedDB.");
-        return false;
-    }
-    return true;
-}
+let db;
 
-function useIndexedDb(databaseName, storeName, method, object) {
-    return new Promise((resolve, reject) => {
-        const request = window.indexedDB.open(databaseName, 1);
-        let db,
-            tx,
-            store;
+const request = indexedDB.open("budget", 1);
 
-        request.onupgradeneeded = function (e) {
-            const db = request.result;
-            db.createObjectStore(storeName, { keyPath: "_id" });
-        };
-
-        request.onerror = function (e) {
-            console.log("There was an error");
-        };
-
-        request.onsuccess = function (e) {
-            db = request.result;
-            tx = db.transaction(storeName, "readwrite");
-            store = tx.objectStore(storeName);
-
-            db.onerror = function (e) {
-                console.log("error");
-            };
-            if (method === "put") {
-                store.put(object);
-            } else if (method === "get") {
-                const all = store.getAll();
-                all.onsuccess = function () {
-                    resolve(all.result);
-                };
-            } else if (method === "delete") {
-                store.delete(object._id);
-            }
-            tx.oncomplete = function () {
-                db.close();
-            };
-        };
+request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    db.createObjectStore("pending", {
+        autoIncrement: true,
     });
-}
-function saveRecord(record){
-    useIndexedDb("budgetNameDB", "transaction", "put", {...record,_id:Math.floor(Math.random()*10000)});
 };
 
-// Listen for app coming back online
-window.addEventListener('online', checkDatabase);
+request.onsuccess = (event) => {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = (event) => {
+    console.log("Whoops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json",
+                },
+            })
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+
+window.addEventListener("online", checkDatabase);
