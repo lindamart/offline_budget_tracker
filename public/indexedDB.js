aacb2de9b845a6bd88bdb26606698a476abe3471
Linda Martinez <lindanmjw@gmail.com>
2022-02-04 20:26:03 -0500

    updating comments in code

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -4,39 +4,41 @@ const indexedDB =
     window.webkitIndexedDB ||
     window.msIndexedDB ||
     window.shimIndexedDB;
-
+// Create DB
 let db;
-
+// Request to open budget DB w/ vers1
 const request = indexedDB.open("budget", 1);
-
+// When upgrade needed grab db and create a collection/table
 request.onupgradeneeded = (event) => {
     const db = event.target.result;
     db.createObjectStore("pending", {
         autoIncrement: true,
     });
 };
-
+// when db successfuly opened/created get result and if online check and send
 request.onsuccess = (event) => {
     db = event.target.result;
     if (navigator.onLine) {
         checkDatabase();
     }
 };
-
+// if error log whoops
 request.onerror = (event) => {
     console.log("Whoops! " + event.target.errorCode);
 };
-
+// save record (called by index.js) create transaction with pending table >> target pending >> add to table
 function saveRecord(record) {
     const transaction = db.transaction(["pending"], "readwrite");
     const store = transaction.objectStore("pending");
     store.add(record);
 }
-
+// connect to pending table >> >>  >> >>  post to create 
 function checkDatabase() {
     const transaction = db.transaction(["pending"], "readwrite");
+    // get everything added to table while offline 
     const store = transaction.objectStore("pending");
     const getAll = store.getAll();
+    // check table and is there anything if yes (>0) send transactions in array to server to bulk upload
     getAll.onsuccess = () => {
         if (getAll.result.length > 0) {
             fetch("/api/transaction/bulk", {
@@ -47,6 +49,7 @@ function checkDatabase() {
                     "Content-Type": "application/json",
                 },
             })
+            // when successful connect to BD again and clear out since already added
                 .then(() => {
                     const transaction = db.transaction(["pending"], "readwrite");
                     const store = transaction.objectStore("pending");
@@ -55,5 +58,5 @@ function checkDatabase() {
         }
     };
 }
-
+// when back online run checkDB
 window.addEventListener("online", checkDatabase);
