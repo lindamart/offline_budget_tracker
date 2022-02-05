b44f7d07a503530ea5c8626c2c87945c16a537c8
Linda Martinez <lindanmjw@gmail.com>
2022-02-05 09:29:11 -0500

    updating db

--- server.js
+++ server.js
@@ -15,20 +15,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-// This is from MongoAtlas Insturctions
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-//   }
-// );
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
